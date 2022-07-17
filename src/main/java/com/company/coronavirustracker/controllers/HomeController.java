321843e18200fe0b3a7f73f6dbc376c985815427
Oddinx <ghostingphamton@gmail.com>
2022-07-17 13:59:23 -0500

    Revert "make it better"
    This reverts commit b685fd45c1a8c13dcf0d09f29b3fb68bda9d1141.

--- src/main/java/com/company/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/company/coronavirustracker/controllers/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
 
     @Autowired
     CoronaVirusDataService coronaVirusDataService;
-    @GetMapping("/")
+    @GetMapping("/asd")
     public String home(Model model) {
         List<LocationStats> allStats = coronaVirusDataService.getAllStats();
         int totalReportedCases = allStats.stream().mapToInt(stats -> stats.getLatestTotalCases()).sum();
